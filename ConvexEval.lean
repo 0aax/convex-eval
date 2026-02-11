59d39c3ff71e5fb7d452a3f1c9906d25432aefd2
Audrey <ahx@mit.edu>
2026-02-11 14:52:41 -0800

    Locally updated everything.

--- ConvexEval.lean
+++ ConvexEval.lean
@@ -1,6 +1,6 @@
-import ConvexEval.definitions
 import ConvexEval.CAMA_chap_3
 import ConvexEval.FCA_chap_B
 import ConvexEval.FCA_chap_C
 import ConvexEval.FCA_chap_D
 import ConvexEval.FCA_chap_E
+import ConvexEval.definitions
